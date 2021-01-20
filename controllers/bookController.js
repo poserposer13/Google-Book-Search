09087081b1c075aac9c4e125e5cf79d7dbdb7d46
Sean Kempf <poserposer@me.com>
2021-01-19 18:21:45 -0700

    Update the controller to maatch routes that will be easier to use

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,40 +1,37 @@
-const db = require('../models');
-const router = require('express').Router();
+const db = require("../models");
 
-router.get('/', function(req, res) {
-    db.Book.find(req.query)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-});
+module.exports = {
+  findAll: function(req, res) {
+    db.Book
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Book
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Book
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Book
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Book
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
 
-
-router.get('/:id',  function(req, res) {
-    db.Book.findById(req.params.id)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-});
-
-
-router.post('/',  function(req, res) {
-    db.Book.create({
-        ...req.body
-    })
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-});
-
-router.put('/:id', function(req, res) {
-    db.Book.findByIdAndUpdate(req.params.id, req.body)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-});
-
-
-router.delete('/:id', function(req, res) {
-    db.Book.findByIdAndDelete(req.params.id)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-});
-
-
-module.exports = router;
