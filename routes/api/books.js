da0602e559475196367c54873e40dd2045e655ec
Sean Kempf <poserposer@me.com>
2021-01-19 19:27:58 -0700

    Installed axios, created api for client

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const bookController = require("../../controllers/bookController");
 
-router.route("/")
+router.route("/books")
   .get(bookController.findAll)
   .post(bookController.create);
 
 router
-  .route("/:id")
+  .route("/books/:id")
   .get(bookController.findById)
   .put(bookController.update)
   .delete(bookController.remove);
