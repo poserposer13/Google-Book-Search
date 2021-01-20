1c328dea6bba1cada724c70885cec32b6a5f256a
Sean Kempf <poserposer@me.com>
2021-01-19 18:12:47 -0700

    Added more npm libraries, set up the App.js in the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+
 import "./App.css";
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+      <Route exact path = {["/", "/search"]}>
+        <Search />
+      </Route>
+      <Route exact path= "/saved">
+        <Saved />
+      </Route>
     </div>
+    </Router>
   );
 }
 
